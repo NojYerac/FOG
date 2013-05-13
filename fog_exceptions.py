fdbb0944b6dd7d58173a6c19c9f02b3d9c8149e1
Noj Yerac <noj.yerac@gmail.com>
2013-05-13 02:16:58 -0500

    Wrote FOG_Request method... Still need to test/debug & write docs.

---
+++ fog_exceptions.py
@@ -0,0 +1,6 @@
+
+class FOG_Exception(Exception):
+    pass
+
+class FOG_AlreadyGotOneError(FOG_Exception):
+    pass
--- fog_request.py
+++ fog_request.py
@@ -15,9 +15,50 @@ OPTIONS:
 """
 
 import os, os.path, sys
-from glob import glob
-from getopt import gnu_getopt
+import pickle
+from hashlib import md5 as md5hash
+from urllib import request
+from urllib.parse import urlencode
+from fog_exceptions import FOG_AlreadyGotOneError
 
+API_LOC = 'api.key' # Enter the file location of the api key file
+CACHE_DIR = '.cache'
+
+class FOGRequest():
+    def __init__(self, data, endpoint='http://capitolwords.org/api/text.json', API_LOC=API_LOC):
+        self.data = data
+        self.endpoint = endpoint
+        with open(API_LOC, 'rt') as apikey_file:
+            self.apikey = apikey_file.read(32)
+        self.modifyData(apikey=self.apikey)
+        self.urlEncodeData()
+    def urlEncodeData(self):
+        self.url_data = urlencode(self.data).encode()
+        self.getHash()
+    def modifyData(self, **new_data):
+        self.data.update(new_data)
+        self.urlEncodeData()
+    def replaceAllData(self, **new_data):
+        self.data = {'api_key':self.api_key}
+        self.modifyData(new_data)
+    def __fetchResult(self):
+        self.result = request.openurl(self.endpoint, self.url_data)
+    def getHash(self):
+        self.md5_digest = md5hash(self.url_data).hexdigest()
+    def request(self):
+        cache_file_name = os.path.join(self.cache_dir, self.md5_digest)
+        try:
+            if os.path.isdir(self.cache_dir):
+                if os.path.isfile(cache_file_name):
+                    raise(FOG_AlreadyGotOneError())
+            else: os.mkdir(self.cache_dir)
+            self.fetchResult()
+            with open(cache_file_name, 'wb') as cache_file:
+                pickle.dump(self.result, cache_file)
+        except FOG_AlreadyGotOneError:
+            with open(cache_file_name, 'rb') as cache_file:
+                self.result = pickle.load(cache_file)
+            
 def failout(message):
     print(message, file = sys.stderr)
     usage()
@@ -37,6 +78,8 @@ def main():
 # PSUDOCODE HERE. A LITTLE PREPERATION GOES A LONG WAY!!!!!!!
 
 if __name__ == '__main__':
+from getopt import gnu_getopt
+
     main()
 
     
